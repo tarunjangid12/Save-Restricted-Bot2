c6e66b388fc1c01911106600f734f1d714b36cc4
tarunjangid12 <146462019+tarunjangid12@users.noreply.github.com>
2024-05-01 13:59:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -167,6 +167,8 @@ def handle_private(message: pyrogram.types.messages_and_media.message.Message, c
 		if "Document" == msg_type:
 			try:
 				thumb = None
+			except Exception as e:
+	                    print(e)
 			
 			bot.send_document(message.chat.id, file, thumb=thumb, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
 			if thumb != None: os.remove(thumb)
@@ -174,6 +176,8 @@ def handle_private(message: pyrogram.types.messages_and_media.message.Message, c
 		elif "Video" == msg_type:
 			try: 
 				thumb = None
+			except Exception as e:
+		            print(e)
 
 			bot.send_video(message.chat.id, file, duration=msg.video.duration, width=msg.video.width, height=msg.video.height, thumb=thumb, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
 			if thumb != None: os.remove(thumb)
